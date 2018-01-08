9327f8549861d46e59609adc41e83378c86540e0
Agata <agata.golebiewska@yahoo.co.uk>
2018-01-08 15:23:34 +0000

    Enable top up functionality

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,14 @@
 class Oystercard
+#controls the user balance
+
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
 require 'oystercard'
 
 describe Oystercard do
-  
+  subject(:oystercard) { described_class.new }
+
+  it 'has a initial balance of 0' do
+    expect(oystercard.balance).to eq(0)
+  end
+
+  it 'checks if the card has been topped-up' do
+    expect { oystercard.top_up 1 }.to change { oystercard.balance }.by 1
+  end
 end
